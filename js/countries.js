0521dcc0c2df288edf5651ddc2f5fa2999b4e300
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-21 23:28:34 +0600

    finished

--- js/countries.js
+++ js/countries.js
@@ -5,15 +5,18 @@ const loadCountries = () => {
 };
 
 const displayCountries = countries =>{
-     console.log(countries);
+     console.log(countries[0]);
      const countriesHTML = countries.map(country => getCountryHTML(country))
-     console.log(countriesHTML);
+     // console.log(countriesHTML);
+     const container = document.getElementById('countries');
+     container.innerHTML = countriesHTML.join(' ');
 }
 
 const getCountryHTML = country => {
      return `
-          <div>
+          <div class="country">
           <h2>${country.name.common}</h2>
+          <img src="${country.flags.png}">
           </div>
      `
 }

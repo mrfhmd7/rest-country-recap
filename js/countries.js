cdce0b89f47034cb262f14dcd81e8787559dc104
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-22 22:08:13 +0600

    some file added

--- js/countries.js
+++ js/countries.js
@@ -4,7 +4,7 @@ const loadCountries = () => {
           .then(data => displayCountries(data))
 };
 
-const displayCountries = countries =>{
+const displayCountries = countries => {
      console.log(countries[0]);
      const countriesHTML = countries.map(country => getCountryHTML(country))
      // console.log(countriesHTML);
@@ -12,13 +12,40 @@ const displayCountries = countries =>{
      container.innerHTML = countriesHTML.join(' ');
 }
 
-const getCountryHTML = country => {
+/* const getCountryHTML = country => {
+     //option-1
+     const {name, flags} = country;
+     return `
+          <div class="country">
+          <h2>${name.common}</h2>
+          <img src="${flags.png}">
+          </div>
+     `
+} */
+
+
+const getCountryHTML = ({name, capital, population, area, region, flags}) => {
+     //option-2
+     return `
+          <div class="country">
+          <h2>${name.common}</h2>
+          <h3>Capital: ${capital}</h3>
+          <p>Population: ${population}</p>
+          <p>Area: ${area} km²</p>
+          <p>Region: ${region}</p>
+          <img src="${flags.png}">
+          </div>
+     `
+}
+
+
+/* const getCountryHTML = (country) => {
      return `
           <div class="country">
           <h2>${country.name.common}</h2>
           <img src="${country.flags.png}">
           </div>
      `
-}
+} */
 
 loadCountries();
